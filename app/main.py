8de198e28d930bd25791214279b5be35a0e2a0ad
Ian Lim <ian.c.lim12@gmail.com>
2025-07-31 15:47:41 -0400

    Load models, update gitignore

--- app/main.py
+++ app/main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI
 from app.api.routes import router
-from app.ml.model import ml_model
+from app.ml.model import model_registry, ModelOptions
 
 app = FastAPI(
     title="ML Model Serving API",
     description="A FastAPI application for serving machine learning models",
-    version="1.0.0"
+    version="1.0.0",
 )
+
 
 # Load the model on startup
 @app.on_event("startup")
 async def startup_event():
-    ml_model.load_model()
+    model_registry.get_models(ModelOptions.iris).load_model()
+    model_registry.get_models(ModelOptions.diabetes).load_model()
+
 
 # Include the API routes
 app.include_router(router, prefix="/api/v1", tags=["predictions"])
+
 
 @app.get("/")
 async def root():
